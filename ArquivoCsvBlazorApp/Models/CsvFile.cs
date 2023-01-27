93e0ba657130f3aaa3753f99ad7ccaa408456a8d
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2023-01-27 17:03:13 -0500

    Add project files.

---
+++ ArquivoCsvBlazorApp/Models/CsvFile.cs
@@ -0,0 +1,34 @@
+﻿namespace ArquivoCsvBlazorApp.Models
+{
+    public class CsvFile
+    {
+        public CsvFile(
+            long ccnpjCpf,
+            long cflialCnpj,
+            long cctrlCnpjCpf,
+            long cpfOriginal,
+            int ctpoPssoa,
+            string rest,
+            string grau)
+        {
+            CCNPJ_CPF = ccnpjCpf;
+            CFLIAL_CNPJ = cflialCnpj;
+            CCTRL_CNPJ_CPF = cctrlCnpjCpf;
+            CPF_ORIGINAL = cpfOriginal;
+            CTPO_PSSOA = ctpoPssoa;
+            REST = rest;
+            GRAU = grau;
+        }
+
+        public long CCNPJ_CPF { get; set; }
+        public long CFLIAL_CNPJ { get; set; }
+        public long CCTRL_CNPJ_CPF { get; set; }
+        public long CPF_ORIGINAL { get; set; }
+        public int CTPO_PSSOA { get; set; }
+        public string REST { get; set; }
+        public string GRAU { get; set; }
+
+    }
+
+
+}
---
+++ ArquivoCsvBlazorApp/Models/ProgressiveStreamContent.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+
+public class ProgressiveStreamContent : StreamContent
+{
+    private readonly Stream _fileStream;
+    private readonly int _maxBuffer = 1024 * 4;
+
+    public ProgressiveStreamContent(Stream stream, int maxBuffer, Action<long, double> onProgress) : base(stream)
+    {
+        _fileStream = stream;
+        _maxBuffer = maxBuffer;
+        OnProgress += onProgress;
+    }
+
+    public event Action<long, double> OnProgress;
+
+    protected async override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+    {
+        var buffer = new byte[_maxBuffer];
+        var totalLength = _fileStream.Length;
+        long uploaded = 0;
+
+        while (true)
+        {
+            using (_fileStream)
+            {
+                var length = await _fileStream.ReadAsync(buffer, 0, _maxBuffer);
+                if (length <= 0)
+                {
+                    break;
+                }
+
+                uploaded += length;
+                var percentage = Convert.ToDouble(uploaded * 100 / _fileStream.Length);
+
+                await stream.WriteAsync(buffer);
+
+                OnProgress?.Invoke(uploaded, percentage);
+
+                await Task.Delay(250);
+            }
+        }
+    }
+
+}
